233e9bfb8391d28e7f6bf1b2f405cc08e604c8ad
Lewis Vine <lew.vine@gmail.com>
2024-07-09 08:56:27 -0400

    Added variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,9 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = currentResult + 10 * 3;
+
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 -1';
+
+outputResult(currentResult, calculationDescription);
+
