1c43018711c036cf6517b34d7a8dad27d96956b0
Lewis Vine <lew.vine@gmail.com>
2024-07-09 09:39:12 -0400

    working with functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = currentResult + 10 * 3;
+function addTwoNumbers(numOne, numTwo){
+    const result = numOne + numTwo;
+    return result;
+}
+
+currentResult = addTwoNumbers(1,2);
+
 
 let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 -1';
 
